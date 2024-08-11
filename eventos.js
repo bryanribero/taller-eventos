7a7dd9b78b15830a6c2efe7aa11cb1a8995cc45c
Bryan <bryanribero@hotmail.com>
2024-08-11 07:13:24 -0300

    Agregue un evento para que el div tire una alerta

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let contenedor = document.getElementsByTagName('div')[0];
+
+    contenedor.addEventListener('click', function(){
+        alert('Hola! Soy el div');
+    });
+});
